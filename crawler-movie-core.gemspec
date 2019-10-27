48fe84eb31ebadd5535a4965c0afce89b98fa371
Jonathan PHILIPPE <pretrine@gmail.com>
2019-10-27 14:14:32 +0100

    Adds crawler core

--- crawler-movie-core.gemspec
+++ crawler-movie-core.gemspec
@@ -30,4 +30,5 @@ Gem::Specification.new do |spec|
   spec.add_development_dependency 'rake', '~> 10.0'
   spec.add_runtime_dependency 'activesupport', '>= 3.0'
   spec.add_runtime_dependency 'levenshtein-ffi', '>= 1.0'
+  spec.add_runtime_dependency 'crawler-core', '>= 0.2'
 end
--- lib/crawler/movie.rb
+++ lib/crawler/movie.rb
@@ -1,9 +1,12 @@
 require 'active_support/core_ext/hash/keys'
 require 'active_support/inflector'
-require 'levenshtein-ffi'
+require 'crawler/base'
+require 'crawler/utils'
 
 module Crawler
   module Movie
+    include Base
+
     PROVIDERS = []
     SCORES = {}
 
@@ -17,27 +20,15 @@ module Crawler
       end
     end
 
-    def self.configure
-      yield self
-    end
-
-    def self.transliterate(string)
-      ActiveSupport::Inflector.transliterate(string.gsub(/[:\-.,!?]/, ' ').strip.gsub(/\s+/, ' ')).downcase
-    end
-
     def self.search(query, year: nil)
       movies = PROVIDERS.flat_map do |provider_name|
         camelized = ActiveSupport::Inflector.camelize("crawler/movie/providers/#{provider_name.to_s}")
         klass = ActiveSupport::Inflector.constantize(camelized)
-        movies = klass.search(transliterate(query))
+        movies = klass.search(Utils.transliterate(query))
 
         movies.map do |movie|
           provider_score = SCORES[provider_name] || 0.5
-          query_transliterated = transliterate(query)
-          title_transliterated = transliterate(movie[:title])
-          levenshtein_distance = Levenshtein.distance(query_transliterated, title_transliterated)
-          max_size = [query_transliterated.size, title_transliterated.size].max.to_f
-          title_score = (max_size - levenshtein_distance) / max_size
+          title_score = Utils.levenshtein_score(query, movie[:title])
           year_score = 1.0 unless year
           year_score ||= movie[:release_date] && year.to_s == movie[:release_date].year.to_s ? 1.0 : 0.9
 
@@ -49,7 +40,7 @@ module Crawler
       end
 
       movies.group_by do |movie|
-        [transliterate(movie[:data][:title]), movie[:data][:release_date] && movie[:data][:release_date].year]
+        [Utils.transliterate(movie[:data][:title]), movie[:data][:release_date] && movie[:data][:release_date].year]
       end
     end
 
